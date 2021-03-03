2c2f3630b39ad1bd01acb395b1e7e42aa479c9b6
yamaguchitakaaki <kenmei.takaaki@gmail.com>
2021-03-03 20:04:48 +0900

    山口画面追加

---
+++ mstf/MatchYamaguchiController.swift
@@ -0,0 +1,29 @@
+//
+//  MatchYamaguchiController.swift
+//  mstf
+//
+//  Created by 山口賢明 on 2021/03/03.
+//
+
+import UIKit
+
+class MatchYamaguchiController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
