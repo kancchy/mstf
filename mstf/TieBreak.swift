dedc787347a97d33fddcf45eebeb94720d833085
hiroto.tajiri <hiroto.tajiri.ts@mixi.co.jp>
2021-03-10 19:58:23 +0900

    domain_create

---
+++ mstf/TieBreak.swift
@@ -0,0 +1,14 @@
+//
+//  TieBreak.swift
+//  mstf
+//
+//  Created by hiroto.tajiri.ts on 2021/03/10.
+//
+
+import Foundation
+
+class TieBreak{
+    private (set) var gamePoint:Int!;
+    private (set) var gamePointCountTeamA:Int? ;
+    private (set) var gamePointCountTeamB:Int? ;
+}
