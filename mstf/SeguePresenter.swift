668b301954aeabe16b58c3f4084bc8a744c8b9f7
hiroto.tajiri <hiroto.tajiri.ts@mixi.co.jp>
2021-05-26 21:26:50 +0900

    ViewControllerからMatchControllerに画面遷移させない（コード削除）

--- mstf/SeguePresenter.swift
+++ mstf/SeguePresenter.swift
@@ -40,77 +40,40 @@ class SeguePresenter:DelegateProtocol{
 
         } else if segue.identifier == "matchSegue" {
 
-                // 遷移先ViewCntrollerの取得
-                let nextView = segue.destination as! MatchController;
-                // 値の設定
-                nextView.player1Name = (delegate?.player1.text!)!
-                nextView.player2Name = (delegate?.player2.text!)!
-                nextView.player3Name = (delegate?.player3.text!)!
-                nextView.player4Name = (delegate?.player4.text!)!
+            // 遷移先ViewCntrollerの取得
+            let nextView = segue.destination as! MatchController;
+            // 値の設定
+            nextView.player1Name = (delegate?.player1.text!)!
+            nextView.player2Name = (delegate?.player2.text!)!
+            nextView.player3Name = (delegate?.player3.text!)!
+            nextView.player4Name = (delegate?.player4.text!)!
                 
-                let dateformatter = DateFormatter()
-                dateformatter.dateFormat = "yyyy/MM/dd"
-                nextView.date = dateformatter.string(from: (delegate?.startDate.date)!);
-                
+            let dateformatter = DateFormatter()
+            dateformatter.dateFormat = "yyyy/MM/dd"
+            nextView.date = dateformatter.string(from: (delegate?.startDate.date)!);
         } else if segue.identifier == "matchKanda" {
             // 遷移先ViewCntrollerの取得
             let nextView = segue.destination as! MatchKandaController;
-            // 値の設定
-            if delegate != nil{
-                // ViewControllerからスコア集計画面に遷移するボタンが押された場合
-                nextView.receivedPlayer1Name = (delegate?.player1.text!)!
-                nextView.receivedPlayer2Name = (delegate?.player2.text!)!
-                nextView.receivedPlayer3Name = (delegate?.player3.text!)!
-                nextView.receivedPlayer4Name = (delegate?.player4.text!)!
-                
-                let dateformatter = DateFormatter()
-                dateformatter.dateFormat = "yyyy/MM/dd"
-                nextView.startDate = dateformatter.string(from: (delegate?.startDate.date)!);
-            }else{
-                // ViewDetailControllerからスコア集計画面に遷移するボタンが押された場合
-                nextView.receivedPlayer1Name = (viewDetailDelegate?.receivedPlayer1Name.description)!
-                nextView.receivedPlayer2Name = (viewDetailDelegate?.receivedPlayer2Name.description)!
-                nextView.receivedPlayer3Name = (viewDetailDelegate?.receivedPlayer3Name.description)!
-                nextView.receivedPlayer4Name = (viewDetailDelegate?.receivedPlayer4Name.description)!
-                nextView.serverTeamName = (viewDetailDelegate?.serverPlayerTeam.description)!
-                nextView.receivedSetCount = (viewDetailDelegate?.playSetCount.description)!
-                nextView.receivedGameCOunt = (viewDetailDelegate?.playGameCount.description)!
-            }
-            
+            nextView.receivedPlayer1Name = (viewDetailDelegate?.receivedPlayer1Name.description)!
+            nextView.receivedPlayer2Name = (viewDetailDelegate?.receivedPlayer2Name.description)!
+            nextView.receivedPlayer3Name = (viewDetailDelegate?.receivedPlayer3Name.description)!
+            nextView.receivedPlayer4Name = (viewDetailDelegate?.receivedPlayer4Name.description)!
+            nextView.serverTeamName = (viewDetailDelegate?.serverPlayerTeam.description)!
+            nextView.receivedSetCount = (viewDetailDelegate?.playSetCount.description)!
+            nextView.receivedGameCOunt = (viewDetailDelegate?.playGameCount.description)!
         } else if segue.identifier == "matchTajiri" {
 
             // 遷移先ViewCntrollerの取得
             let nextView = segue.destination as! MatchTajiriController;
 
-            // 値の設定
-            if delegate != nil{
-                // ViewControllerからスコア集計画面に遷移するボタンが押された場合
-                nextView.inputPlayerName1 = (delegate?.player1.text!)!
-                nextView.inputPlayerName2 = (delegate?.player2.text!)!
-                let dateformatter = DateFormatter()
-                dateformatter.dateFormat = "yyyy/MM/dd"
-                nextView.startDate = dateformatter.string(from: (delegate?.startDate.date)!);
-            }else{
-                // ViewDetailControllerからスコア集計画面に遷移するボタンが押された場合
-                nextView.inputPlayerName1 = (viewDetailDelegate?.serverPlayerName.description)!
-                print("セット数：" + (viewDetailDelegate?.playSetCount.description)!)
-                print("ゲーム数：" + (viewDetailDelegate?.playGameCount.description)!)
-            }
+            nextView.inputPlayerName1 = (viewDetailDelegate?.serverPlayerName.description)!
+            print("セット数：" + (viewDetailDelegate?.playSetCount.description)!)
+            print("ゲーム数：" + (viewDetailDelegate?.playGameCount.description)!)
         } else if segue.identifier == "matchYamaguchi" {
             
             // 遷移先ViewCntrollerの取得
             let nextView = segue.destination as! MatchYamaguchiController;
 
-            // 値の設定
-            if delegate != nil{
-                // ViewControllerからスコア集計画面に遷移するボタンが押された場合
-                nextView.inputPlayerName1 = (delegate?.player1.text!)!
-                nextView.inputPlayerName2 = (delegate?.player2.text!)!
-                let dateformatter = DateFormatter()
-                dateformatter.dateFormat = "yyyy/MM/dd"
-                nextView.startDate = dateformatter.string(from: (delegate?.startDate.date)!);
-            }else{
-                // ViewDetailControllerからスコア集計画面に遷移するボタンが押された場合
                 nextView.inputPlayerName1 = (viewDetailDelegate?.receivedPlayer1Name.description)!
                 nextView.inputPlayerName2 = (viewDetailDelegate?.receivedPlayer2Name.description)!
                 nextView.inputPlayerName3 = (viewDetailDelegate?.receivedPlayer3Name.description)!
@@ -118,7 +81,6 @@ class SeguePresenter:DelegateProtocol{
                 nextView.serverTeamName = (viewDetailDelegate?.serverPlayerTeam.description)!
                 print("セット数：" + (viewDetailDelegate?.playSetCount.description)!)
                 print("ゲーム数：" + (viewDetailDelegate?.playGameCount.description)!)
-            }
         }
     }
 }
